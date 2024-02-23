98933f114c6d137a94548f652d5a125f2f26e4fb
Lisette Svan <svanlisette@gmail.com>
2024-02-23 14:41:13 +0100

    commented out some stuff

--- routes/catone.js
+++ routes/catone.js
@@ -17,13 +17,13 @@ export default catoneRouther;
 
 
 
-function Cat(name, age, color) {
-    this.name = name,
-    this.age = age,
-    this.color = color
-}
+// function Cat(name, age, color) {
+//     this.name = name,
+//     this.age = age,
+//     this.color = color
+// }
 
-const Pepzi = new Cat ("Pepzi,  4 , grey");
-const  Sirap =  new Cat("Sirap, 10, orange");
+// const Pepzi = new Cat ("Pepzi,  4 , grey");
+// const  Sirap =  new Cat("Sirap, 10, orange");
 
-const catsRule = [];
+// const catsRule = [];
